bc9177cd9b2488f254755731108ee19d4a7fa92d
tonberry140 <tonberry140@gmail.com>
2022-02-09 20:15:03 +0900

    Script linked to index and barebones pseudocode

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// function to randomly choose 1 2 or 3 
+//convert 1 2 3 to rock paper and scissors
+
+// implement user input of rock paper and scissors, convert to lowercase
+// compare rock paper scissors to see who has one
+
+//record wins / losses
+//loop game until player or computer has won/lost 5 times
