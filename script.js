6667806d36b2a504241bf92492b87f05fa801478
tonberry140 <tonberry140@gmail.com>
2022-02-09 21:28:06 +0900

    Computer choice function added

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 
 // function to randomly choose 1 2 or 3 
+function computerPlay() {
+    let numberChoice = Math.floor((Math.random()*3)+1);
 //convert 1 2 3 to rock paper and scissors
+    switch (numberChoice) {
+        case  1 : return "rock";
+        case 2 : return "paper";
+        case 3 : return "scissors"; 
+    }
+
+}
+
 
 // implement user input of rock paper and scissors, convert to lowercase
 // compare rock paper scissors to see who has one
